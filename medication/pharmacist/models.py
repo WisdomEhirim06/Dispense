617be3daa281868b702e22a1255e0ad2b3221c12
WisdomEhirim06 <erichenrywisel@gmail.com>
2024-02-19 01:37:30 +0100

    latest addition to views and models.py

--- medication/pharmacist/models.py
+++ medication/pharmacist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Drug(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    brand_name = models.CharField(max_length=100, blank=True, null=True)
+    composition = models.TextField
+    indications = models.TextField
+    interactions = models.TextField
+    dosage = models.TextField
+    side_effects = models.TextField
+
+    def __str__(self):
+        return self.name
+
+class DrugHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    search_query = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.search_query}"
--- medication/pharmacist/views.py
+++ medication/pharmacist/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
